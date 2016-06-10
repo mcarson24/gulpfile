9630bf4af798aa9ad3d52768741029b1e51e3ed7
Matthew Carson <matthew24carson@gmail.com>
2016-06-10 10:18:04 -0400

    Add gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var phpspec = require('gulp-phpspec');
+var notify = require('gulp-notify');
+
+gulp.task('test', function() {
+    gulp.src('spec/**/*.php')
+        .pipe(phpspec('', { clear: true, notify: true }))
+        .on('error', notify.onError({
+            title: 'Darn!',
+            message: 'Your tests failed Matthew:-(',
+        }))
+        .pipe(notify({
+            title: "Woopee!",
+            message: 'It\'s so easy bein\' green!'
+        }));
+});
+
+gulp.task('watch', function() {
+    gulp.watch(['spec/**/*.php', 'src/**/*.php'], ['test']);
+});
+
+gulp.task('default', ['test', 'watch']);
